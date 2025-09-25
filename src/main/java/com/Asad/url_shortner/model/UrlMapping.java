b3aef9f22c3dba2f3401eb5f9b5484a96cb59732
Asad Ibrahim Shaikh <shaikhasad9665@gmail.com>
2025-09-26 02:10:47 +0530

    my first controller

---
+++ src/main/java/com/Asad/url_shortner/model/UrlMapping.java
@@ -0,0 +1,22 @@
+package com.Asad.url_shortner.model;
+
+// This line declares that our class belongs to the 'model' package.
+// This helps in organizing our project, separating data models from controllers, services, etc.
+/**
+ * This class represents the core data model for our URL Shortener. It is a POJO
+ * (Plain Old Java Object), which means it's a simple object not bound by any
+ * special restrictions or frameworks at this stage.
+ *
+ * Each instance of this class will eventually correspond to a single row in our
+ * database table, holding the crucial link between an original, long URL and the
+ * short code we generate for it.
+ *
+ * In the upcoming tasks, we will use JPA annotations (like @Entity, @Id, @Column)
+ * to transform this simple POJO into a powerful JPA Entity that Spring Data
+ * can manage and persist to the database automatically.
+ */
+public class UrlMapping {
+    // This class is currently empty. We will add fields to it in the next tasks,
+    // such as 'id', 'originalUrl', and 'shortCode', which will become the
+    // columns of our database table.
+}
